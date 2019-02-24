39a3b760ce8c965bde256744538d9e18c64edbab
Clivern <hello@clivern.com>
2019-02-24 20:44:39 +0100

    update datetime class

--- pyumetric/provider/utils.py
+++ pyumetric/provider/utils.py
@@ -8,19 +8,17 @@ from datetime import datetime, timedelta
 
 class Datetime_Utils():
 
-    def datetime(self, tz="UTC", diff_days=0):
-
+    def __init__(self, tz="UTC", diff_days=0):
         dt = datetime.now(pytz.timezone(tz))
 
         if diff_days > 0:
             dt += timedelta(days=diff_days)
         elif diff_days < 0:
             dt -= timedelta(days=(-1 * diff_days))
+        self.__dt = dt
 
-        return dt
+    def iso(self):
+        return self.__dt.isoformat()
 
-    def to_iso(self, dt):
-        return dt.isoformat()
-
-    def format(self, dt, format):
-        return dt.strftime(format)
+    def format(self, format):
+        return self.__dt.strftime(format)
--- tests/unit/provider/test_utils.py
+++ tests/unit/provider/test_utils.py
@@ -9,15 +9,15 @@ class TestUtils(unittest.TestCase):
     def test_utils(self):
         self.assertEqual(
             (datetime.now(pytz.timezone("UTC")) + timedelta(days=1)).strftime("%m/%d/%Y, %H:%M:%S"),
-            Datetime_Utils().datetime("UTC", 1).strftime("%m/%d/%Y, %H:%M:%S")
+            Datetime_Utils("UTC", 1).format("%m/%d/%Y, %H:%M:%S")
         )
         self.assertEqual(
             (datetime.now(pytz.timezone("UTC")) - timedelta(days=1)).strftime("%m/%d/%Y, %H:%M:%S"),
-            Datetime_Utils().datetime("UTC", -1).strftime("%m/%d/%Y, %H:%M:%S")
+            Datetime_Utils("UTC", -1).format("%m/%d/%Y, %H:%M:%S")
         )
         self.assertEqual(
             (datetime.now(pytz.timezone("UTC"))).strftime("%m/%d/%Y, %H:%M:%S"),
-            Datetime_Utils().datetime("UTC", 0).strftime("%m/%d/%Y, %H:%M:%S")
+            Datetime_Utils("UTC", 0).format("%m/%d/%Y, %H:%M:%S")
         )
 
 
